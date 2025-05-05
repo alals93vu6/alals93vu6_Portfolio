dae27691d41f7502a1b59768f3ead23b946f48da
alals93uv6 <a0987915131@gmail.com>
2025-05-05 09:57:19 +0800

    Added Switch Page

--- JavaScript/index/UICtrl/HeroPageCtrl.js
+++ JavaScript/index/UICtrl/HeroPageCtrl.js
@@ -1,3 +1,5 @@
+var pageNumber = 1;
+
 export async function StartAnimator() {
     document.getElementsByClassName("more-link")[0].style.opacity = 0;
     await delay(500);
@@ -8,6 +10,43 @@ export async function StartAnimator() {
     document.getElementById("hero-desc").style.opacity = 1;
     await delay(500);
     document.getElementsByClassName("more-link")[0].style.opacity = 0.8;
+
+    setTimeout(function () {
+        SwitchPageAnimator();
+    }, 5000);
+}
+
+async function SwitchPageAnimator() {
+    document.getElementsByClassName("more-link")[0].style.opacity = 0;
+    await delay(300);
+    document.getElementById("hero-desc").style.opacity = 0;
+    await delay(150);
+    document.getElementById("hero-title").style.opacity = 0;
+    await delay(500);
+    document.getElementsByClassName("bg-mask")[0].style.opacity = 1;
+    await delay(3300);
+    HeroPageDetected();
+    document.getElementsByClassName("bg-mask")[0].style.opacity = 0;
+    await delay(1000);
+    document.getElementById("hero-title").style.opacity = 1;
+    await delay(300);
+    document.getElementById("hero-desc").style.opacity = 1;
+    await delay(500);
+    document.getElementsByClassName("more-link")[0].style.opacity = 0.8;
+
+    setTimeout(function () {
+        SwitchPageAnimator();
+    }, 5000);
+}
+
+function HeroPageDetected() {
+    if (pageNumber == 1) {
+        pageNumber++;
+        document.body.style.backgroundImage = "url('../../Assets/image/index/HeroPage_BackGroundB.png')";
+      } else {
+        pageNumber = 1;
+        document.body.style.backgroundImage = "url('../../Assets/image/index/HeroPage_BackGroundA.png')";
+      }
 }
 
 function delay(ms) {
