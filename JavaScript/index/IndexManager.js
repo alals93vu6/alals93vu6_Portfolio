a21fcb34841bc188b27d602f126455b605a97afe
alals93uv6 <a0987915131@gmail.com>
2025-05-05 11:53:42 +0800

    Added back to top

--- JavaScript/index/IndexManager.js
+++ JavaScript/index/IndexManager.js
@@ -1,7 +1,16 @@
 import * as UI from './UIManager.js';
 //import * as User from './UserControl.js';
 
-window.onload = function () {
+//網頁載入時
+document.addEventListener("DOMContentLoaded", () => {
     UI.OnPageStart();
-    setupUserControl();
-};
+    //setupUserControl();
+  
+    const backBtn = document.getElementById("backToTop");
+    if (backBtn) {
+      backBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      });
+    }
+  });
+
